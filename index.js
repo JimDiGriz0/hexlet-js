68767e392b93cd0d363061887c21732ed08dcec7
digriz <vakhitov.ms28@gmail.com>
2024-04-27 21:55:22 +0300

    Add new files

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import indi from "./src/index.js"
 console.log("Hello, Hexlet!");
 import _ from "lodash";
 console.log(_.last(["one", "two"]));
+
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export default () => console.log("Hello, Hexlet!");
+import _ from "lodash";
+console.log(_.last(["one", "two"]));
